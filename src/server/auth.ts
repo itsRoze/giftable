342abe48f6131531947a209d07b46344ab26f277
elewis9989 <elewis9989@gmail.com>
2023-02-27 15:45:32 -0500

    setup email auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,11 +1,11 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { type GetServerSidePropsContext } from 'next';
 import {
   getServerSession,
-  type NextAuthOptions,
   type DefaultSession,
+  type NextAuthOptions,
 } from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import EmailProvider from 'next-auth/providers/email';
 import { env } from '~/env.mjs';
 import { prisma } from '~/server/db';
 
@@ -47,19 +47,24 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    EmailProvider({
+      server: {
+        host: env.EMAIL_SERVER || 'https://localhost:3000',
+        port: 587,
+        auth: {
+          user: 'apikey',
+          pass: env.EMAIL_PASSWORD || '',
+        },
+      },
+      from: env.EMAIL_FROM || 'default@default.com',
+      ...(env.NODE_ENV !== 'production'
+        ? {
+            sendVerificationRequest({ url }) {
+              console.log('LOGIN LINK', url);
+            },
+          }
+        : {}),
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
