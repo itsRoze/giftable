605b0c7fc232aebe351d5194bc9170db22c1b444
elewis9989 <elewis9989@gmail.com>
2023-03-10 15:15:21 -0500

    pull friends from db

--- src/server/api/routers/friends.ts
+++ src/server/api/routers/friends.ts
@@ -5,7 +5,7 @@ import {
   protectedProcedure,
   publicProcedure,
 } from '~/server/api/trpc';
-import { createFakeBirthdays, createFakeFriends } from '../../../lib/helpers';
+import { createFakeBirthdays } from '../../../lib/helpers';
 
 export const friendsRouter = createTRPCRouter({
   getUpcomingBirthdays: publicProcedure.input(z.number()).query(({ input }) => {
@@ -13,9 +13,38 @@ export const friendsRouter = createTRPCRouter({
       items: createFakeBirthdays(input),
     };
   }),
-  getFriends: protectedProcedure.query(() => {
+  getFriends: protectedProcedure.query(async ({ ctx }) => {
+    const userId = ctx.session.user.id;
+
+    const friendshipData = await ctx.prisma.friend.findMany({
+      where: {
+        OR: [
+          {
+            requestedId: userId,
+          },
+          {
+            requesterId: userId,
+          },
+        ],
+      },
+      include: {
+        users: true,
+      },
+    });
+
+    const arr = [];
+    for (const friendship of friendshipData) {
+      if (friendship.users[0] && friendship.users[1]) {
+        arr.push(
+          friendship.users[0]?.id !== userId
+            ? friendship.users[0]
+            : friendship.users[1]
+        );
+      }
+    }
+
     return {
-      friends: createFakeFriends(),
+      friends: arr,
     };
   }),
 });
