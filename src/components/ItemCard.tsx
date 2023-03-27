41a8ce1c382549f3aea7fecc5488e07815f6edc2
elewis9989 <elewis9989@gmail.com>
2023-03-27 15:59:01 -0400

    fix item card url button

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -9,16 +9,18 @@ const ItemCard: React.FC<Props> = ({ item }) => {
     <div className="card-bordered card w-72 border-gray-400 bg-white	">
       <div className="card-body">
         <h2 className="card-title block truncate text-pink-400">{item.name}</h2>
-        <div className="card-actions ">
-          <a
-            href={item.url}
-            target="_blank"
-            className="btn border-none bg-blue-300 text-white"
-            rel="noreferrer"
-          >
-            Visit
-          </a>
-        </div>
+        {item.url && (
+          <div className="card-actions ">
+            <a
+              href={item.url}
+              target="_blank"
+              className="btn border-none bg-blue-300 text-white"
+              rel="noreferrer"
+            >
+              Visit
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
